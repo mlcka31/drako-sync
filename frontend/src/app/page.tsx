8d1e01d3fc40746ec5c7e265b96786fc3616ebeb
Mlcka13 <modayoda@rambler.ru>
2025-03-22 18:53:41 -0300

    [update] hero

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -37,15 +37,18 @@ export default function HomePage() {
       openConnectModal && openConnectModal();
       return;
     }
-    sendMessage(message, utils.formatEther(120));
+    if (!messagePrice) {
+      // toast.error("Message price is not set");
+      return;
+    }
+    sendMessage(message, utils.formatEther(messagePrice));
   };
   return (
     <main className="flex h-full w-full flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       {!isStarted ? (
-        <div className="container z-10 mt-[100px] flex flex-col items-center justify-center gap-12 px-4 py-16">
+        <div className="container z-10 mt-[100px] flex h-[90vh] flex-col items-center justify-center gap-12 px-4 py-16">
           <Logo className="h-20 w-full" />
           <div className="z-50 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-            {/* <ConnectBtn /> */}
             <button
               className="h-[80px] w-full bg-transparent text-[32px] md:text-[80px]"
               onClick={() => {
@@ -55,45 +58,10 @@ export default function HomePage() {
               Start
             </button>
           </div>
-          {/* <SendMoneyButton /> */}
-          {/* <Profile /> */}
-          {/* <div>
-          {messages?.map((item) => {
-            return (
-              <div key={item.timestamp}>
-                <p>{item.content}</p>
-              </div>
-            );
-          })}
-        </div> */}
-          {/* <div>
-          <p>Game State: {gameState}</p>
-          <p>Prize Pool: {prizePool}</p>
-          <p>Message Price: {messagePrice}</p>
-        </div>
-
-        <button
-          onClick={() => sendMessage("Hello world!", utils.formatEther(120))}
-        >
-          Send Message
-        </button> */}
-          {/* 
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white lg:static lg:h-auto lg:w-auto lg:bg-none dark:from-black dark:via-black">
-          <Link href="/start" className="mt-4 block">
-            <button className="rounded-lg bg-blue-600 px-4 py-2 font-medium text-white transition duration-200 hover:bg-blue-700">
-              Go to Start Page
-            </button>
-          </Link>
-        </div> */}
         </div>
       ) : (
         <div className="container z-10 mt-[100px] flex h-full max-w-[800px] flex-grow flex-col gap-12">
           <Navbar />
-
-          {/* <p>Game State: {gameState}</p>
-          <p>Prize Pool: {prizePool}</p>
-          <p>Message Price: {messagePrice}</p> */}
-          {/* <div className="flex-1"> */}
           <Chat
             prizePool={prizePool?.toString() || "0"}
             messagePrice={messagePrice?.toString() || "0"}
@@ -110,7 +78,6 @@ export default function HomePage() {
             className="h-full w-full"
             isDisabled={isLoading || gameState !== GameState.UserAction}
           />
-          {/* </div> */}
         </div>
       )}
     </main>
