6b9f54fb7a5a93a8c5975f06c5ae9168a2886a2f
Mlcka13 <modayoda@rambler.ru>
2025-03-22 20:23:45 -0300

    [fix]

--- frontend/src/components/FontTest.tsx
+++
@@ -1,9 +0,0 @@
-export default function FontTest() {
-  return (
-    <div className="space-y-4 p-4">
-      <h1 className="text-3xl">This should use Superstar font by default</h1>
-      <p className="font-geist">This uses Geist font explicitly</p>
-      <p className="font-superstarOrig">This uses SuperstarOrig font</p>
-    </div>
-  );
-}
