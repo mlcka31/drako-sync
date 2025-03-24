0cfb92fed00535be266d1e9b9f4f918951ad4316
Mlcka13 <modayoda@rambler.ru>
2025-03-23 21:22:40 -0300

    [fix]

--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -1,5 +1,5 @@
 export const contractAddress =
-  (process.env.PUBLIC_NEXT_SMART_CONTRACT_ADDRESS as `0x${string}`) ||
+  (process.env.NEXT_PUBLIC_SMART_CONTRACT_ADDRESS as `0x${string}`) ||
   "0x2334cA4eDc697824BeeAb473045DE1f56d07754C";
 
 export const rpcUrl = "http://server1.xprts.xyz:8545";
@@ -7,9 +7,9 @@ export const rpcUrl = "http://server1.xprts.xyz:8545";
 export const githubRepo = "https://github.com/mlcka31/drako-sync";
 
 export const backendAttestation =
-  (process.env.PUBLIC_NEXT_BACKEND_ATTESTATION_URL as `0x${string}`) ||
+  (process.env.NEXT_PUBLIC_BACKEND_ATTESTATION_URL as `0x${string}`) ||
   "https://github.com/mlcka31/zk-dungeon-backend";
 
 export const backendApp =
-  (process.env.PUBLIC_NEXT_BACKEND_APP_URL as `0x${string}`) ||
+  (process.env.NEXT_PUBLIC_BACKEND_APP_URL as `0x${string}`) ||
   "https://github.com/mlcka31/zk-dungeon-backend";
